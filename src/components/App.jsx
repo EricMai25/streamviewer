d75342715f76443aec27b59ddb57e4cbb82ac72f
ericmai25 <maieric25@gmail.com>
2019-05-17 13:37:58 -0700

    established all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
-import axios from 'axios'
+import React from "react";
+import Chat from "./Chatbox.jsx";
+import Nav from "./Nav.jsx";
+import Home from "./Home.jsx";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: null,
+      messages: [],
+      username: "",
+      icon: ""
+    };
+  }
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {}
-    }
+  componentDidMount() {
+    fetch("/video", {
+      method: "GET",
+      headers: { "Content-type": "application/json" }
+    })
+      .then(data => {
+        return data.json();
+      })
+      .then(videodata => {
+        this.setState({
+          video: videodata.items[0].id.videoId
+        });
+      });
+  }
+  responseGoogle(response) {
+    console.log(response);
+  }
+  addMessage(message) {
+    let mbox = this.state.messages;
+    let totalmessage = mbox.push(message);
+    this.setState({
+      messages: totalmessage
+    });
+    console.log(this.state.messages);
+  }
 
-    componentDidMount(){
-        axios.get('GET https://www.googleapis.com/youtube/v3/liveBroadcasts')
-        .then((response)=>{
-            console.log(response)
-        })
-        .catch(error => {
-            console.log( 'Error : '+error)
-        })
-    }
-
-    render(){
-        return(
-            <div>
-                Hello World
-            </div>
-        )   
-    }
+  render() {
+    return (
+      <div>
+        <Nav gRes={this.responseGoogle.bind(this)} />
+        <Home video={this.state.video} />
+        <Chat msgs={this.state.messages} add={this.addMessage.bind(this)} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
