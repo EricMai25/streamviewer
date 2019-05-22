6df6b5530ab5709d194246b7e48cfc70004ddde5
ericmai25 <maieric25@gmail.com>
2019-05-21 20:38:20 -0700

    Ready for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
   },
-  mode: "development",
+  mode: "production",
   devtool: "inline-source-map",
   module: {
     rules: [
