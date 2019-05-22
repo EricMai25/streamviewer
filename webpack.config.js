725eec2c31c5ce4b3c0a0d9759d5df82d2539320
ericmai25 <maieric25@gmail.com>
2019-05-21 20:14:43 -0700

    checking

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
   },
-  mode: "development",
+  mode: "production",
   node: {
     fs: 'empty',
     net: 'empty',
